5dc058174887864ba514e86d7c5ff174df6794aa
Richard Goodhue <richard.goodhue@gmail.com>
2025-02-10 13:39:13 -0500

    SharerNotification Added

---
+++ src/main/ts/org/derecalliance/derec/impl/DeRecSharerNotification.ts
@@ -0,0 +1,51 @@
+//create class that holds necessary properties for sharer's notification
+//methods return specific notification details
+
+export class SharerNotification {
+    private notificationType: SharerNotificationType;
+    private notificationMessage: string;
+    private notificationVersion: DeRecVersion | null;
+    private helperStatus: DeRecHelperStatus | null;
+    private secretId: DeRecSecret;
+    private severity: SharerNotificationSeverity;
+
+    constructor(
+        notificationType: SharerNotificationType,
+        notificationMessage: string,
+        notificationVersion: DeRecVersion | null,
+        helperStatus: DeRecHelperStatus | null,
+        secretId: DeRecSecret,
+        severity: SharerNotificationSeverity
+    ) {
+        this.notificationType = notificationType;
+        this.notificationMessage = notificationMessage;
+        this.notificationVersion = notificationVersion;
+        this.helperStatus = helperStatus;
+        this.secretId = secretId;
+        this.severity = severity;
+    }
+
+    getType(): SharerNotificationType {
+        return this.notificationType;
+    }
+
+    getMessage(): string {
+        return this.notificationMessage;
+    }
+
+    getVersion(): DeRecVersion | null {
+        return this.notificationVersion;
+    }
+
+    getHelper(): DeRecHelperStatus | null {
+        return this.helperStatus;
+    }
+
+    getSecret(): DeRecSecret {
+        return this.secretId;
+    }
+
+    getSeverity(): SharerNotificationSeverity {
+        return this.severity;
+    }
+}
